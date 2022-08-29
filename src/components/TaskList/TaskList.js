bbc2ffcc4bf2c5aff90f64d7a17b39c76116ccc2
Disneystark <77645004+Disneystark@users.noreply.github.com>
2022-08-29 13:36:45 +0300

    Отображение кнопки при вводе текста в инпут

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -3,7 +3,13 @@ import styles from "./TaskList.module.css";
 import { LIST_BACKLOG } from "../../const/listTypes";
 
 export const TaskList = (props) => {
-  const [isInputShown, setInputShown] = useState(false); //false
+  const [isInputShown, setInputShown] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (event) => {
+    const value = event.target.value;
+    setInputValue(value);
+  };
 
   const handleAddClick = () => {
     if (props.listType === LIST_BACKLOG) {
@@ -23,15 +29,26 @@ export const TaskList = (props) => {
         {props.taskList.map((item) => {
           return <div className={styles.list_task_main}>{item.name}</div>;
         })}
-        {isInputShown && <input className={styles.input_Task} />}
+
+        {isInputShown && (
+          <input
+            value={inputValue}
+            onChange={handleInputChange}
+            className={styles.input_Task}
+          />
+        )}
+
         {!isInputShown && (
           <button onClick={handleAddClick} className={styles.button_add}>
             + Add card
           </button>
         )}
+
         {isInputShown && <button onClick={handleCancelClick}>Cancel</button>}
 
-        <button className={styles.button_submit}>Submit</button>
+        {isInputShown && inputValue.length > 0 && (
+          <button className={styles.button_submit}>Submit</button>
+        )}
       </div>
     </div>
   );
