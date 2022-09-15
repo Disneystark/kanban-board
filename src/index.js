42178ce5cf0877bd2bb7f42dedfb7fc05ae880d4
Disneystark <77645004+Disneystark@users.noreply.github.com>
2022-09-15 17:25:45 +0300

    подключил react router

--- src/index.js
+++ src/index.js
@@ -4,11 +4,19 @@ import "./reset.css";
 import "./index.css";
 import App from "./components/App/App";
 import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
